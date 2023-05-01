b6551dce9f8b3762954fded07b3d621de99e0a76
wifi-user <54284414+wifi-user@users.noreply.github.com>
2023-05-02 06:07:34 +0800

    更新 get_img.py

--- get_img.py
+++ get_img.py
@@ -50,7 +50,7 @@ for filename in os.listdir(directory):
                 os.makedirs(img_dir, exist_ok=True)
 
                 # 下载图片
-                response = requests.get(img_url,headers=headers, stream=True)
+                response = requests.get(img_url,headers=headers)
                 with open(img_path, 'wb') as f:
                     shutil.copyfileobj(response.raw, f)
                     print(img_path,'完')
