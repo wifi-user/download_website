b3532d38256c41cc63874437a39fd3d8e4990a6f
wifi-user <54284414+wifi-user@users.noreply.github.com>
2023-05-01 20:42:19 +0800

    更新 get_img.py

--- get_img.py
+++ get_img.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 import shutil
 import re
 import urllib.parse#url编码
-# 设置请求头 
- headers = { 
-     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+# 设置请求头
+headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
 # 获取程序所在的目录路径
 directory = os.path.dirname(os.path.abspath(__file__))
 
