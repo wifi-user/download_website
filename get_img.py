292dbb171d36ae4b124e82e2190133069ddc0c87
wifi-user <54284414+wifi-user@users.noreply.github.com>
2023-05-01 20:58:30 +0800

    更新 get_img.py

--- get_img.py
+++ get_img.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import shutil
 import re
-import urllib.parse#url编码
+import urllib.parse 
 # 设置请求头
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
